04f541fb0f2fe3e66ae553236d1336a61cbfc59f
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 00:10:24 +0100

    staff-info

--- dbHandler.js
+++ dbHandler.js
@@ -1,11 +1,22 @@
 const { Pool } = require('pg');
 
+const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-  host: 'localhost',
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT,
+// });
 
 module.exports = pool;
