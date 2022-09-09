916f181a60da488bdfc0d101b22d0dcb5cd81e2a
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 00:58:02 +0100

    staff-info

--- client/src/actions/staff.js
+++ client/src/actions/staff.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 export const getStaff = async () =>
-  await axios.post(`${process.env.REACT_APP_API}/staff`);
+  await axios.post(`https://staff-info.herokuapp.com/staff`);
 
 export const addNewStaff = async (staff) =>
-  await axios.post(`${process.env.REACT_APP_API}/add`, staff);
+  await axios.post(`https://staff-info.herokuapp.com/add`, staff);
 
 export const deleteStaff = async (staffId) =>
-  await axios.post(`${process.env.REACT_APP_API}/delete/${staffId}`);
+  await axios.post(`https://staff-info.herokuapp.com/delete/${staffId}`);
 
 export const editStaff = async (staffId, data, token) =>
-  await axios.put(`${process.env.REACT_APP_API}/update/${staffId}`, data, {
+  await axios.put(`https://staff-info.herokuapp.com/update/${staffId}`, data, {
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
