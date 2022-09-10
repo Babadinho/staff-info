3ad4a2439c0cc12c673980b5edbccf2f9a8e41de
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 02:12:09 +0100

    staff-info

--- dbHandler.js
+++ dbHandler.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
-const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
-
-const isProduction = process.env.NODE_ENV === 'production';
+const connectionString =
+  'postgres://vqlzztvvbevxbe:843f6ff5886f290fcd8d2b9e9028b8815f5a954b6213c7d429f300bcfae2699d@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/dbvdud00nom7b1';
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString,
 });
 
 module.exports = pool;
--- server.js
+++ server.js
@@ -6,20 +6,6 @@ const fs = require('fs');
 require('dotenv').config();
 const app = express();
 
-//Cors config
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested, Content-Type, Accept Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
-    return res.status(200).json({});
-  }
-  next();
-});
-
 // app middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
