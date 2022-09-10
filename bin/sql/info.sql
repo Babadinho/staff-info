6d44c030fdaa862e0bc7a2b0763665fc1be7a983
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 16:39:04 +0100

    staff-info

--- bin/sql/info.sql
+++ bin/sql/info.sql
@@ -10,3 +10,8 @@
 -- );
 
 SELECT * FROM staff
+
+-- INSERT INTO staff 
+--     (staff_name, staff_email, staff_phone, staff_image, department)
+-- VALUES 
+--     ('Addison Jennings', 'addison.jennings@example.com', '(704) 836-6949', 'https://randomuser.me/api/portraits/women/65.jpg', 1);
--- dbHandler.js
+++ dbHandler.js
@@ -1,23 +1,23 @@
 const { Pool } = require('pg');
 
-// const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
+const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
 
-// const isProduction = process.env.NODE_ENV === 'production';
-
-// const pool = new Pool({
-//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-//   ssl: {
-//     require: true,
-//     rejectUnauthorized: false,
-//   },
-// });
+const isProduction = process.env.NODE_ENV === 'production';
 
 const pool = new Pool({
-  host: 'ec2-3-219-19-205.compute-1.amazonaws.com',
-  user: 'mcgxmwfsvwutzh',
-  database: 'dbrh2bh3dpns8q',
-  password: 'cadc5356dc0d0ff7c30ba660d848a69f7bca66ab6ec613c967dab7093a9187f5',
-  port: '5432',
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: process.env.USER,
+//   database: process.env.DATABASE,
+//   password: process.env.PASSWORD,
+//   port: process.env.PORT,
+// });
 
 module.exports = pool;
