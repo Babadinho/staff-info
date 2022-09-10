19162faa75939430fe5dc0d4975e165bc2ee0c41
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 01:52:17 +0100

    staff-info

--- server.js
+++ server.js
@@ -6,10 +6,24 @@ const fs = require('fs');
 require('dotenv').config();
 const app = express();
 
+//Cors config
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested, Content-Type, Accept Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // app middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(cors({ origin: 'https://staff-info.vercel.app' }));
+app.use(cors({ origin: process.env.CLIENT_URL }));
 
 //route middleware
 fs.readdirSync('./routes').map((routes) =>
