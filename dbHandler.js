c3e5b0e8bda6cd7f49d313b3ba027442dd60ae22
Donald Iloekwe <donytex4u@gmail.com>
2022-09-10 15:21:35 +0100

    staff-info

--- dbHandler.js
+++ dbHandler.js
@@ -1,29 +1,13 @@
 const { Pool } = require('pg');
 
-const connectionString = `postgresql://${process.env.USER}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.PORT}/${process.env.DATABASE}`;
-
-const isProduction = process.env.NODE_ENV === 'production';
-
-if (connectionString) {
-  console.log('Connected locally');
-} else if (isProduction) {
-  console.log('Connected in producton');
-}
+const connectionString = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString,
   ssl: {
     require: true,
     rejectUnauthorized: false,
   },
 });
-
-// const pool = new Pool({
-//   host: 'localhost',
-//   user: process.env.USER,
-//   database: process.env.DATABASE,
-//   password: process.env.PASSWORD,
-//   port: process.env.PORT,
-// });
 
 module.exports = pool;
