72f8787b75fe8f5e787c0544c65e57a389bb64f4
Donald Iloekwe <donytex4u@gmail.com>
2022-10-18 01:29:56 +0100

    make it better

--- bin/sql/info.sql
+++ bin/sql/info.sql
@@ -3,6 +3,7 @@ CREATE TABLE staff(
     staff_name VARCHAR(50) NOT NULL,
     staff_email VARCHAR(50) UNIQUE NOT NULL,
     staff_phone VARCHAR(50) UNIQUE NOT NULL,
+    staff_address VARCHAR(255) UNIQUE NOT NULL,
     staff_image VARCHAR(200),
     department serial REFERENCES department(department_id)
 );
--- bin/sql/user.sql
+++ bin/sql/user.sql
@@ -6,9 +6,14 @@ CREATE TABLE user_data(
     password VARCHAR(500) UNIQUE NOT NULL
 );
 
+INSERT INTO user_data (name, email, password)
+VALUES('admin', 'admin@staffinfo.com', 'admin');
+INSERT INTO user_data (name, email, password)
+VALUES('user', 'user@staffinfo.com', 'user');
+
 UPDATE user_data 
 SET status = 'admin' 
-WHERE  id = 2;
+WHERE  id = 1;
 
 SELECT *
 FROM
